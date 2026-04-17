789a13d195dda619e314c3335bf4e85bcca99648
Yasin Aydas <251250647+aydasYa@users.noreply.github.com>
2026-04-17 11:53:46 +0200

    add text elements for admin page

--- app/components/AdminFeatures.tsx
+++ app/components/AdminFeatures.tsx
@@ -2,11 +2,23 @@
 import { QRCodeSVG } from 'qrcode.react';
 import { useState } from 'react';
 
+// TypeScript needs "Types by Inference", no other solution then adding Props object
 type Props = {
     firstname: string
     lastname: string
 }
 
 export default function AdminFeatures({ firstname, lastname }: Props) {
-    
+    const [url, setUrl] = useState('');
+
+    return (
+        <main>
+            <h1>Admin</h1>
+            <p>Willkommen, {firstname}, {lastname}</p>
+
+            <div>
+                // qrcode logic
+            </div>
+        </main>
+    )
 }
