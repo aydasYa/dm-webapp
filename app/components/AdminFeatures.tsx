77a8baa1ca2aa10e914a136e0e3d833782a9bce3
Yasin Aydas <251250647+aydasYa@users.noreply.github.com>
2026-04-17 11:50:06 +0200

    (feat) add AdminFeature page.tsx for login redirect

---
+++ app/components/AdminFeatures.tsx
@@ -0,0 +1,12 @@
+// ADMIN - Features component
+import { QRCodeSVG } from 'qrcode.react';
+import { useState } from 'react';
+
+type Props = {
+    firstname: string
+    lastname: string
+}
+
+export default function AdminFeatures({ firstname, lastname }: Props) {
+    
+}
