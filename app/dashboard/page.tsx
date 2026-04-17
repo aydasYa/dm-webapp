4a3e99f7bbe8beef4ca7351c47143f5096bf2051
Yasin Aydas <251250647+aydasYa@users.noreply.github.com>
2026-04-17 14:03:46 +0200

    (feat) Add drivers list for adming page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,22 +8,36 @@ import UserFeatures from '@/app/components/UserFeatures'
 export const dynamic = 'force-dynamic'
 
 export default async function DashboardPage() {
-  // 1. Check Supabase session
-  const supabase = await createClient()
-  const { data } = await supabase.auth.getClaims()
-  if (!data?.claims) redirect('/login')
+	// 1. Check Supabase session
+	const supabase = await createClient()
+	const { data } = await supabase.auth.getClaims()
+	if (!data?.claims) redirect('/login')
 
-  // 2. Fetch your Prisma user by supabaseId
-  const user = await prisma.user.findUnique({
-    where:  { supabaseId: data.claims.sub },
-    select: { role: true, firstname: true, lastname: true, status: true }
-  })
+	// 2. Eingeloggten User holen
+	const user = await prisma.user.findUnique({
+		where:  { supabaseId: data.claims.sub },
+		select: { role: true, firstname: true, lastname: true, status: true }
+	})
 
-  if (!user) redirect('/login')
+	if (!user) redirect('/login')
 
-  // Render based on role admin or user (tow-truck-driver)
-  if (user.role === Role.ADMIN) return <AdminFeatures firstname={ user.firstname } lastname={ user.lastname } />
-  
-  // else -> User (Abschlepper/tow-truck-driver)
-  return <UserFeatures firstname={ user.firstname } lastname={ user.lastname } status={ user.status } />
+	// 3. Alle fahrer aus der Datenbank holen
+	const drivers = await prisma.user.findMany({
+		where: { role: Role.TOW_TRUCK_DRIVER },
+		select: { id: true, firstname: true, lastname: true }
+	});
+
+	// 4. Rolle prüfen und richtige Component rendern
+	if (user.role === Role.ADMIN) {
+		return <AdminFeatures firstname={ user.firstname } lastname={ user.lastname } />
+	}
+	
+	// else -> User (Abschlepper/tow-truck-driver)
+	return (
+		<UserFeatures 
+		firstname={ user.firstname }
+		lastname={ user.lastname } 
+		status={ user.status } 
+		/>
+	)
 }
