18c3deaa8c2f5f0f17e22a1c809c30b3448c6f36
Yasin Aydas <251250647+aydasYa@users.noreply.github.com>
2026-04-17 11:49:06 +0200

    (style) add margin and font-styling

--- app/ui/say-hello.tsx
+++ app/ui/say-hello.tsx
@@ -1,8 +1,8 @@
 export function SayHello() {
     return (
         <main>
-            <h1 className="text-4xl">Wilkommen auf der Hauptseite der Dashbaord-beta WebApp!</h1>
-            <p>/login - einloggen als Admin oder User (Abschlepper)</p>
+            <h1 className="text-4xl m-8">Wilkommen auf der Hauptseite der Dashbaord-beta WebApp!</h1>
+            <p  className="text-lg m-8"><span className="font-bold">/login</span> - page.tsx zum einloggen als Admin oder User (Abschlepper)</p>
         </main>
     )
 }
