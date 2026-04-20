a5dc33ca40a76e42a3b45450e738e738ddab59e3
Yasin Aydas <251250647+aydasYa@users.noreply.github.com>
2026-04-20 13:00:34 +0200

    Remove deprecated code

--- app/ui/say-hello.tsx
+++
@@ -1,8 +0,0 @@
-export function SayHello() {
-    return (
-        <main>
-            <h1 className="text-4xl m-8">Wilkommen auf der Hauptseite der Dashbaord-beta WebApp!</h1>
-            <p  className="text-lg m-8"><span className="font-bold">/login</span> - page.tsx zum einloggen als Admin oder User (Abschlepper)</p>
-        </main>
-    )
-}
