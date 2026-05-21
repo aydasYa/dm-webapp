297d1e6e93b08376483567d8bf43d2d0f7cd8dc4
Yasin Aydas <251250647+aydasYa@users.noreply.github.com>
2026-05-21 14:22:50 +0200

    (refactor): add render dashboard or loginpage instead greeting page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,13 @@
-import Link from "next/link"
+import { redirect } from "next/navigation"
+import { createClient } from "@/lib/supabase/server"
 
+export const dynamic = "force-dynamic"
 
-export default function Home() {
-    return (
-        <div className="m-8 h-full">
-            <div>
-                <h1 
-                className="text-3xl"
-                >
-                    Willkommen auf dem Abschlepper Dashboard!
-                </h1>
-                
-                {/* Testbereich */}
-                <p>
-                    Eine App von <span className="text-green-500 font-bold">Dein Motorschaden
-                    </span>
-                </p>
-            </div>
+export default async function Home() {
+    const supabase = await createClient()
+    const { data } = await supabase.auth.getClaims()
 
-            {/* Registrierung & Login per Link */}
-            <Link href="/login" className="mt-6 inline-block bg-green-500 rounded-full px-6 py-3">Zum Login</Link>
-        </div>
-    )
+    if (data?.claims) redirect("/dashboard")
+    // Wenn keiner eingeloggt ist -> zum Login
+    redirect("/login")
 }
