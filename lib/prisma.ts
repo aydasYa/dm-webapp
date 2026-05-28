02e7c8d4d445dde48c862c6d0fd409299841dc95
Yasin Aydas <251250647+aydasYa@users.noreply.github.com>
2026-05-28 18:32:05 +0200

    fix: reuse prisma client in development

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,18 @@
-import { PrismaClient } from "@/src/generated/prisma/client"; 
-import { PrismaPg } from "@prisma/adapter-pg"; 
+import { PrismaClient } from "@/src/generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
 
-
-// each comment-code not need for supabase to run, just for the next.js (enable later)
-// const globalForPrisma = global as unknown as {
-//   prisma: PrismaClient; 
-// }; 
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient
+}
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL, 
-}); 
+  connectionString: process.env.DATABASE_URL,
+})
 
-// const prisma = globalForPrisma.prisma || new PrismaClient({ adapter, }); 
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
-const prisma = new PrismaClient({ adapter });
-export default prisma; 
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
+
+export default prisma
